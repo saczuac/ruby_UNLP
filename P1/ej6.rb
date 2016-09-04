42f8d14aba4b0decac5a5ac68f726053cf85bf21
sebaesg <sebastian_sanchezg@hotmail.com>
2016-09-04 17:59:37 -0300

    update de ejercicio 6

--- P1/ej6.rb
+++ P1/ej6.rb
@@ -1,10 +1,7 @@
 def contar ( string, sub_string)
   ocurrences = 0
-  string.downcase.split(/[^a-zA-Z]/).each {
-     |word| if word == sub_string
-               ocurrences+=1
-            end }
-  ocurrences
+  string.downcase.split(/[^a-zA-Z]/).select {
+     |word|word == sub_string}.count
 end
 
 # Getting values
@@ -12,3 +9,6 @@ puts 'Please enter a string, and later a word. This program tells you how many t
 string = gets.chomp
 sub_string = gets.chomp
 puts contar(string, sub_string)
+
+
+
